6efa0e4050be37767afa6474c2b1e0b5a56d27c3
chenneal <chenneal@126.com>
2016-04-02 17:04:36 +0800

    medium

--- DistinctSubsequences.cpp
+++ DistinctSubsequences.cpp
@@ -32,3 +32,40 @@ public:
         return dp[s.size()-1][t.size()-1];
     }
 };
+
+/* optimized version */
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        if (s.size() < t.size())    return 0;
+        int dp[s.size()+1][t.size()+1];
+        int num = 0;
+        /* Iintialize the base state */
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == t[0]) {
+                num++;
+            }
+            dp[i][0] = num;
+        }
+        
+        for (int j = 1; j < t.size(); j++) {
+            dp[0][j] = 0;
+        }
+        
+        for (int j = 1; j < t.size(); j++) {
+            for (int i = j; i < s.size(); i++) {
+                if (i == j) {
+                    dp[i][j] = (s[i] == t[j] ? dp[i-1][j-1] : 0);
+                }
+                else {
+                    dp[i][j] = dp[i-1][j]; 
+                    if (s[i] == t[j])
+                        dp[i][j] += dp[i-1][j-1];
+                }
+            }
+        }
+
+        return dp[s.size()-1][t.size()-1];
+    }
+};
