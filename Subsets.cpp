9635c5dede3f7b6b76e5c0a7e03022189751fc45
chenneal <yzhengt@163.com>
2016-04-06 19:49:54 +0800

    medium

---
+++ Subsets.cpp
@@ -0,0 +1,7 @@
+/*  Given a set of distinct integers, nums, return all possible subsets.
+
+Note:
+
+    Elements in a subset must be in non-descending order.
+    The solution set must not contain duplicate subsets.
+*/
