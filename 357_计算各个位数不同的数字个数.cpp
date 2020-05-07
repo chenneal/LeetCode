53becf43f7fdc80fca4145af103073df626130d6
chenneal <chenneal@126.com>
2020-05-07 21:44:53 +0800

    357. 计算各个位数不同的数字个数

---
+++ 357_计算各个位数不同的数字个数.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+      if (n == 0)
+        return 1;
+
+      if (n == 1)
+        return 10;
+
+      int* dp = new int[n + 1];
+
+      dp[0] = 1;
+      dp[1] = 10;
+
+      //递归公式: dp[i] = (11 - i) * (dp[i - 1] - dp[i - 1]) + dp[i - 1]
+      for (int i = 2; i <= n; i++) {
+          dp[i] = (11 - i) * (dp[i - 1] - dp[i - 2]) + dp[i - 1];
+      }
+
+      int result = dp[n];
+
+      return result;
+    }
+};
